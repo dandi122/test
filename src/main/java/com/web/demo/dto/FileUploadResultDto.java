a17563586b26f6dfd676e57af4ae7d5114383ee5
dandi122 <skykjk122@naver.com>
2024-08-23 09:00:01 +0900

    Feat : 8.22 파일업로드 기능 추가

---
+++ src/main/java/com/web/demo/dto/FileUploadResultDto.java
@@ -0,0 +1,25 @@
+package com.web.demo.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 파일 업로드 처리후 응답 결과용 dto => json 형태로 보내짐
+ */
+
+@Getter
+@Setter
+public class FileUploadResultDto {
+	private int code;        // -1: 에러, 1: 성공
+	private String message;  // 메세지
+	
+	@Builder
+	public FileUploadResultDto(int code, String message) {
+		super();
+		this.code = code;
+		this.message = message;
+	}
+	
+
+}
